79dce2b749168dfa266b0f22611f37e4a05bac83
andrew kariuki <andrew4kariuki@gmail.com>
2021-09-08 21:23:17 +0300

    chore(templates): add templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+// exports.createPages = async function ({ actions, graphql }) {
+//   const { data } = await graphql`
+//     query {
+//       allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+//         edges {
+//           node {
+//             id
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `
 
-// You can delete this file if you're not using it
+//   const postsPerPage = 3
+
+//   const numberOfPages = Math.ceil(data.allMdx.edges.length / postsPerPage)
+
+//   //   all blog posts page
+//   Array.from({ length: numberOfPages }).forEach(_, i => {
+//     actions.createPage({
+//       path: i === 0 ? `/` : `/${i + 1}`,
+//       component: require.resolve("./src/templates/allPosts.js"),
+//       context: {
+//         limit: postsPerPage,
+//         skip: i * postsPerPage,
+//         numberOfPages,
+//         currentPage: i + 1,
+//       },
+//     })
+//   })
+
+//   //   single post
+
+//   data.allMdx.edges.forEach(edge => {
+//     const slug = edge.node.frontmatter.slug
+//     const id = edge.node.id
+
+//     actions.createPage({
+//       path: slug,
+//       component: require.resolve("./src/templates/singlePost.js"),
+//       context: {
+//         id,
+//       },
+//     })
+//   })
+// }
