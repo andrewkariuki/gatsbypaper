237ec2d84b7bf06652231e4cda2ef45ddd2ff352
andrew kariuki <andrew4kariuki@gmail.com>
2021-09-10 21:11:31 +0300

    chore(pages): create 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,33 @@
+import { graphql } from "gatsby"
 import * as React from "react"
+import { Container, Content, FeatureImage } from "../components"
+import { H1, P } from "../elements"
 
-const NotFoundPage = () => (
-  <>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const NotFoundPage = ({ data }) => {
+  const featureImage = data.imageSharp.fixed
+  return (
+    <Container>
+      <FeatureImage fixed={featureImage} />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          404: Not Found
+        </H1>
+        <P textAlign="center">
+          You just hit a route that doesn&#39;t exist... the sadness.
+        </P>
+      </Content>
+    </Container>
+  )
+}
 
 export default NotFoundPage
+
+export const pagQuery = graphql`
+  query {
+    imageSharp(fixed: { originalName: { eq: "nature.jpg" } }) {
+      fixed {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`
