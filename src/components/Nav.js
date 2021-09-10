bdcd069661e9a462180a323f932b96049ae192c9
andrew kariuki <andrew4kariuki@gmail.com>
2021-09-10 12:08:00 +0300

    chore(elements): add nav elements

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import { graphql, Link, useStaticQuery } from "gatsby"
+import React from "react"
+import { NavWrapper } from "../elements"
+
+export const Nav = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "gatsby-icon.png" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <NavWrapper>
+      <Link to="/">
+        <img src={data.logo.publicURL} alt="Gatsby Paper Logo" />
+      </Link>
+    </NavWrapper>
+  )
+}
