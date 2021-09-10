5eadd8e4b6e04fac2395e83f208fe3c36d73dccf
andrew kariuki <andrew4kariuki@gmail.com>
2021-09-10 21:00:00 +0300

    chore(code): create syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { Code, Table } from "./src/components"
+import { MDXProvider } from "@mdx-js/react"
+import { preToCodeBlock } from "mdx-utils"
 import Theme from "./src/themes/Theme"
+import "./language-tabs.css"
 
 const GlobalStyles = createGlobalStyle`
 *{
@@ -14,10 +18,23 @@ body,html{
     background-color: ${props => props.theme.colors.light1};
 }
 `
+const components = {
+  table: Table,
+  pre: preProps => {
+    const props = preToCodeBlock(preProps)
+    if (props) {
+      return <Code {...props} />
+    }
+    return <pre {...preProps} />
+  },
+  wrapper: ({ children }) => <>{children}</>,
+}
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-    {element}
-  </ThemeProvider>
+  <MDXProvider components={components}>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      {element}
+    </ThemeProvider>
+  </MDXProvider>
 )
